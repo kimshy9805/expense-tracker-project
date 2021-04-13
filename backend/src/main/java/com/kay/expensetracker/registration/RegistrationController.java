cae7399e018dc78ce78ea7ba1711ef5b875b7f03
Kim SeokHyeon <kimshy9805@gmail.com>
2021-04-13 23:39:25 +0800

    implementing login page

--- backend/src/main/java/com/kay/expensetracker/registration/RegistrationController.java
+++ backend/src/main/java/com/kay/expensetracker/registration/RegistrationController.java
@@ -3,6 +3,7 @@ package com.kay.expensetracker.registration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(path = "api/v1/expense-tracker/registration")
 public class RegistrationController {
@@ -18,15 +19,8 @@ public class RegistrationController {
     //@RequestParam은 "api/v1/expense-tracker/registration/confirm?token=!~~~~~~~"
     //저렇게 confirm뒤에 ? 붙히고 token값을 넣어주는것.
 
-    @GetMapping
-    public String p1() {
-        return "in registration";
-    }
-
     @GetMapping(path = "confirm")
     public String confirm(@RequestParam("token") String token) {
         return registrationService.confirmToken(token);
     }
-
-
 }
